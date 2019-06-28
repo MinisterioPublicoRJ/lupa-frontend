d4d75c312046f6bc2c28fedeb8700347cd93d4cd
Layla <laylacsa@msn.com>
2019-06-28 15:47:18 +0000

    Home is now stateful

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,20 +2,38 @@ import React from 'react';
 
 import './Home.scss';
 import Search from '../search/Search';
-import Map from '../Map/Map'
+import Map from '../Map/Map';
 
-function App() {
-  return (
-    <div className="Home-container">
-      <Search />
-      <div className="Map-view">
-        <Map />
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { buildings: null };
+  }
+
+  handleSearching() {
+    console.log('SEARCHING!')
+  }
+
+  handleMenu() {
+    console.log('Menu')
+  }
+
+  render() {
+    return (
+      <div className="Home-container">
+        <Search
+          searchPressed={() => this.handleSearching()}
+          menuPressed={() => this.handleMenu()}
+        />
+        <div className="Map-view">
+          <Map />
+        </div>
+        <div className="Home-footer">
+          <p>here goes menu and last viewed</p>
+        </div>
       </div>
-      <div className="Home-footer">
-        <p>here goes menu and last viewed</p>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default Home;
