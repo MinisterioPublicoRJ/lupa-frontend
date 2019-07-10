b0bea94a3389267c00ae5bb6dae8121bd3501390
Layla <laylacsa@msn.com>
2019-07-10 15:22:57 -0300

    linting

--- src/components/contents/OrderedList.jsx
+++ src/components/contents/OrderedList.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import './Box.scss'
 import './Lists.scss'
 
-const OrderedList = ({content}) => {
-  return <div className="box list-box">
+const OrderedList = ({ content }) => (
+  <div className="box list-box">
     <div className="list-box--header">
       <h1 className="list-box--title">{content.title}</h1>
-      <img className="list-box--icon" src="" alt="Icone"/>
+      <img className="list-box--icon" src="" alt="Icone" />
     </div>
     <ol className="list-box--list">
-      {
-        content.list.map((itemList, key) => <li key={key} className="list-box--list-item list-box--list-item__ordered">{itemList}</li>)
-      }
+      {content.list.map((itemList, key) => (
+        <li key={key} className="list-box--list-item list-box--list-item__ordered">
+          {itemList}
+        </li>
+      ))}
     </ol>
-    {
-      content && content.image
-      ? <img src={content.image} alt="" className="box--image"/>
-      : null
-    }
+    {content && content.image ? <img src={content.image} alt="" className="box--image" /> : null}
     <p className="list-box--source">{content.source}</p>
   </div>
-}
+)
 
 export default OrderedList
--- src/components/contents/UnorderedList.jsx
+++ src/components/contents/UnorderedList.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import './Box.scss'
 
-const UnorderedList = ({content}) => {
-  return <div className="box list-box">
+const UnorderedList = ({ content }) => (
+  <div className="box list-box">
     <div className="list-box--header">
       <h1 className="list-box--title">{content.title}</h1>
-      <img className="list-box--icon" src="" alt="Icone"/>
+      <img className="list-box--icon" src="" alt="Icone" />
     </div>
     <ol className="list-box--list">
-      {
-        content.list.map((itemList, key) => <li key={key} className="list-box--list-item">{itemList}</li>)
-      }
+      {content.list.map((itemList, key) => (
+        <li key={key} className="list-box--list-item">
+          {itemList}
+        </li>
+      ))}
     </ol>
-    {
-      content && content.image
-      ? <img src={content.image} alt="" className="box--image"/>
-      : null
-    }
+    {content && content.image ? <img src={content.image} alt="" className="box--image" /> : null}
     <p className="list-box--source">{content.source}</p>
   </div>
-}
+)
 
 export default UnorderedList
