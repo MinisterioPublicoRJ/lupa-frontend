5c2f05d20f9d270f4a9c683b428be5716797375e
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2019-07-31 20:11:07 -0300

    Migrating to stateful; properly filtering

--- src/components/contents/FilterableList.jsx
+++ src/components/contents/FilterableList.jsx
@@ -10,41 +10,62 @@ const propTypes = {
   image: PropTypes.node,
   source: PropTypes.string,
 }
-const defaultProps = {
-  image: null,
-  source: null,
-}
 
-const handleFiltering = e => {
-  const value = e.target.value
-  console.log(value)
-}
+class FilterableList extends React.Component {
+  handleFiltering = e => {
+    const value = e.target.value
+    let filteredList = this.props.list
+    if (value) {
+      this.setState({
+        filteredList: filteredList.filter(
+          item => item.dado
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .includes(
+              value
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+            )
+          )
+      })
+    } else {
+      this.setState({filteredList})
+    }
+  }
 
-const FilterableList = ({
-  title, source, list, image
-}) => (
-  <div className="box list-box list-box-filterable">
-    <div className="list-box--header">
-      {title ? <h1 className="list-box--title">{title}</h1> : null}
-      {image ? <img src={image} alt="" className="list-box--icon" /> : null}
-    </div>
-    <input className="list-box-filterable--input" placeholder="🔍 Digite aqui para filtrar" onChange={handleFiltering}/>
-    <div className="list-box-container">
-      <ol className="list-box--list">
-        {list.map(itemList => (
-          <li key={`${itemList.label}-${itemList.dado}`} className="list-box--list-item">
-            {itemList.label ? (
-              <div className="list-box--list-item-label">{itemList.label}</div>
-            ) : null}
-            {itemList.dado ? <div className="list-box--list-item-value">{itemList.dado}</div> : null}
-          </li>
-        ))}
-      </ol>
-    </div>
-    {source ? <div className="list-box--source">{source}</div> : null}
-  </div>
-)
+  componentWillMount(){
+    this.setState({ filteredList: this.props.list })
+  }
+
+  render() {
+    const { title, source, list, image } = this.props
+    const { filteredList } = this.state
+    return (
+      <div className="box list-box list-box-filterable">
+        <div className="list-box--header">
+          {title ? <h1 className="list-box--title">{title}</h1> : null}
+          {image ? <img src={image} alt="" className="list-box--icon" /> : null}
+        </div>
+        <input className="list-box-filterable--input" placeholder="🔍 Digite aqui para filtrar" onChange={this.handleFiltering}/>
+        <div className="list-box-container">
+          <ol className="list-box--list">
+            {filteredList.map(itemList => (
+              <li key={`${itemList.label}-${itemList.dado}`} className="list-box--list-item">
+                {itemList.label ? (
+                  <div className="list-box--list-item-label">{itemList.label}</div>
+                ) : null}
+                {itemList.dado ? <div className="list-box--list-item-value">{itemList.dado}</div> : null}
+              </li>
+            ))}
+          </ol>
+        </div>
+        {source ? <div className="list-box--source">{source}</div> : null}
+      </div>
+    )
+  }
+}
 
 FilterableList.propTypes = propTypes
-FilterableList.defaultProps = defaultProps
 export default FilterableList
