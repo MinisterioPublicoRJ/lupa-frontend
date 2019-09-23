922e896ccc0e8d6340c8734f0b6b68b360146b36
Layla <laylacsa@msn.com>
2019-09-23 15:58:23 -0300

    Component flow completed

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
 import posed from 'react-pose'
 
 import Api from '../api/Api'
@@ -10,8 +10,12 @@ import './Search.scss'
 
 const propTypes = {
   homePressed: PropTypes.func.isRequired,
-  searchPressed: PropTypes.func.isRequired,
 }
+/**
+ * Just a wrapper to animate the menu buttons so that they shrink
+ * when clicked and restore when released
+ * @type {[type]}
+ */
 const WrapperDiv = posed.div({
   pressable: true,
   init: { scale: 1 },
@@ -19,21 +23,69 @@ const WrapperDiv = posed.div({
 })
 
 class Search extends React.Component {
-  state = { open: false }
-
-  handleSearch(inputText) {
-    Api.getSearchData(this.searchCallback, inputText)
-    this.setState({ waiting: true })
-    console.log('input', inputText)
+  constructor(props) {
+    super(props)
+    this.manageSearch = this.manageSearch.bind(this)
+    this.searchCallback = this.searchCallback.bind(this)
+    this.state = { open: false }
   }
 
-  searchCallback(res) {
-    console.log('res', res);
+  /**
+   * Handles input state and debouncing for the API calls
+   * @param  {[event]} event the mouseCick event
+   * @return {[void]}
+   */
+  handleTyping(event) {
+    // fisrt, clean all previous timeouts
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+
+    // now, handle the new input that was typed
+    const query = event.target.value
+    this.timeout = setTimeout(this.manageSearch, 2000, query)
+    this.setState({ query })
+  }
+
+  /**
+   * Triggers the loading state in the component
+   * @param  {[string]} inputText text to be searched
+   * @return {[void]}
+   */
+  manageSearch(inputText) {
+    Api.getSearchData(this.searchCallback, inputText)
+    this.setState({ waiting: true })
+  }
+
+  /**
+   * Handles search results and dismisses loading state
+   * @param  {[array]} response
+   * @return {[void]}
+   */
+  searchCallback(response) {
+    console.log(response)
     this.setState({ waiting: false })
+  }
+
+  /**
+   * Updates state and triggers events on closing
+   * @return {[void]}
+   */
+  handleClosing() {
+    this.setState({ open: false })
+  }
+
+  /**
+   * Updates state and triggers events on opening
+   * @return {[void]}
+   */
+  handleOpening() {
+    this.setState({ open: true })
   }
 
   render() {
     const { homePressed } = this.props
+    const { query, open } = this.state
 
     const placeholder = 'Pesquise Municípios, Prédios e Órgãos'
     return (
@@ -41,11 +93,19 @@ class Search extends React.Component {
         <div className="Search-view">
           <div className="Search-button">
             <WrapperDiv>
-              <MapIcon onClick={homePressed} />
+              {open ? (
+                <FontAwesomeIcon
+                  className="Search-icon"
+                  icon={faTimes}
+                  onClick={() => this.handleClosing()}
+                />
+              ) : (
+                <MapIcon onClick={homePressed} />
+              )}
             </WrapperDiv>
           </div>
           <input
-            onChange={event => this.setState({ query: event.target.value })}
+            onChange={event => this.handleTyping(event)}
             className="Search-input"
             placeholder={placeholder}
             onFocus={() => this.setState({ open: true })}
@@ -55,7 +115,7 @@ class Search extends React.Component {
               <FontAwesomeIcon
                 className="Search-icon"
                 icon={faSearch}
-                onClick={() => this.handleSearch(this.state.query)}
+                onClick={() => this.manageSearch(query)}
               />
             </WrapperDiv>
           </div>
